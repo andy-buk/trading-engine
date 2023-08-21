197c8c642e9325e5c6cccbd396d5845e06d92958
andyb <andy.bukovcan@gmail.com>
2023-08-21 11:49:17 -0400

    Add project files.

---
+++ TradingEngine/TradingEngineServer/TradingEngineServer/TradingEngineServer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using TradingEngineServer.Core.Configuration;
+using TradingEngineServer.Logging;
+
+namespace TradingEngineServer.Core
+{
+    sealed class TradingEngineServer : BackgroundService, ITradingEngineServer
+    {
+        private readonly ITextLogger _logger;
+        private readonly TradingEngineServerConfiguration _tradingEngineServerConfig;
+        public TradingEngineServer(ITextLogger textLogger, IOptions<TradingEngineServerConfiguration> config)
+        {
+            _logger = textLogger ?? throw new ArgumentNullException(nameof(textLogger));
+            _tradingEngineServerConfig = config?.Value ?? throw new ArgumentNullException(nameof(config)); // if it's null then we throw an exception
+        }
+
+        public Task Run(CancellationToken token) => ExecuteAsync(token);
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation($"Started {nameof(TradingEngineServer)}");
+            while (!stoppingToken.IsCancellationRequested)
+            {
+
+            }
+            _logger.LogInformation($"Stopped {nameof(TradingEngineServer)}");
+            return Task.CompletedTask;
+        }
+    }
+}
