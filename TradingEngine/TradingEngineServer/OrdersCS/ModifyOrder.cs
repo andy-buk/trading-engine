01c7c4c7428c3e5e8d176f0e872bfd030f0cdc96
andy b <andy.bukovcan@gmail.com>
2023-08-24 12:14:25 -0400

    added order classes

---
+++ TradingEngine/TradingEngineServer/OrdersCS/ModifyOrder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngineServer.Orders
+{
+    public class ModifyOrder : IOrderCore
+    {
+        public ModifyOrder(IOrderCore orderCore, long modifyPrice, uint modifyQuantity, bool isBuySide)
+        {
+            // properties
+            Price = modifyPrice;
+            Quantity = modifyQuantity;
+            IsBuySide = isBuySide;
+
+            // fields
+            _orderCore = orderCore;
+        }
+        // properties
+        public long Price { get; private set; }
+        public uint Quantity { get; private set; }
+        public bool IsBuySide { get; private set; }
+
+        public long OrderId => _orderCore.OrderId;
+        public string Username => _orderCore.Username;
+        public int SecurityId => _orderCore.SecurityId;
+
+        // methods
+        public CancelOrder ToCancelOrder()
+        {
+            return new CancelOrder(this);
+        }
+
+        public Order ToNewOrder()
+        {
+            return new Order(this);
+        }
+
+        // fields
+        private readonly IOrderCore _orderCore;
+    }
+}
