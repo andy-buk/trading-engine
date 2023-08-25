a4af6a0bae2fa3830ee78cadde640006d24d1ea0
andy b <andy.bukovcan@gmail.com>
2023-08-25 11:06:48 -0400

    added order book

---
+++ TradingEngine/TradingEngineServer/OrdersCS/OrderRecord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngineServer.Orders
+{
+    public record OrderRecord(long OrderId, uint Quantity, long Price, bool IsBuySide, string Username, int SecurityId, uint TheoreticalQueuePosition);
+}
+
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit { };
+}
